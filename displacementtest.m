084432435557eeaf1d1771d928dd23be19251f56
Touqeer Ahmad <touqeer.ahmad@outlook.com>
2017-07-26 12:49:04 -0400

    added test 6

--- displacementtest.m
+++ displacementtest.m
@@ -4,6 +4,7 @@
 
 % Test Commit Nag 3
 % Test Commit Nag 4
+%Test 6
 
 %time = load('C:\Users\touqe\OneDrive\Documents\MATLAB\projecttest\time.txt')
 %accx = load('C:\Users\touqe\OneDrive\Documents\MATLAB\projecttest\accx.txt')
