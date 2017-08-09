6b40b7e2a64d02a21ef3a4cabe39be9292c61da6
Nagrajan23 <c.raj.23@gmail.com>
2017-08-09 12:33:03 -0400

    Deleted Prospect st readings
    No data in these files

--- displacementtest.m
+++ displacementtest.m
@@ -76,8 +76,7 @@ for i = 1:iterFc
 %     [b1,a1] = butter(order,0.1/36,'high');
 %     [b1,a1] = butter(order,fc,'low');
 %     magNoGf=filtfilt(b1,a1,magNoG);
-    magNoGf = filter(Hbp2,magNoG);
-    [b1,a1] = butter(order,[0.016 0.05],'bandpass');
+%     magNoGf = filter(Hbp2,magNoG);
 %       [b1,a1] = butter(order,[0.01 0.05],'bandpass');
 %     [b1,a1] = butter(order,0.1/36,'high');
 %     [b1,a1] = butter(order,fc,'low');
@@ -85,7 +84,8 @@ for i = 1:iterFc
 % xdMODWT = wden(XN,'modwtsqtwolog','s','mln',4,'sym4');
 magNoGf=filtfilt(b1,a1,magNoG);
 
-magNoGf = wden(magNoGf,'modwtsqtwolog','s','mln',8,'sym4');
+% Wavelet filtering
+% magNoGf = wden(magNoGf,'modwtsqtwolog','s','mln',8,'sym4');
 %     magNoGf = filter(Hlp,magNoG);
 %     magNoGf = magNoG;
     % plot(time,accxf,'r',time,accyf,'g',time,acczf,'b');
