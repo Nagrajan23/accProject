c3f5c4866dcef4f7d60dafa6840ad60413160263
Touqeer Ahmad <touqeer.ahmad@outlook.com>
2017-08-09 12:12:33 -0400

    Wavelet

--- displacementtest.m
+++ displacementtest.m
@@ -72,9 +72,14 @@ for i = 1:iterFc
     i
     fc = 0.01/i;
     [b1,a1] = butter(order,[0.016 0.05],'bandpass');
+%       [b1,a1] = butter(order,[0.01 0.05],'bandpass');
 %     [b1,a1] = butter(order,0.1/36,'high');
 %     [b1,a1] = butter(order,fc,'low');
-    magNoGf=filtfilt(b1,a1,magNoG);
+%     magNoGf=filtfilt(b1,a1,magNoG);
+% xdMODWT = wden(XN,'modwtsqtwolog','s','mln',4,'sym4');
+magNoGf=filtfilt(b1,a1,magNoG);
+
+magNoGf = wden(magNoGf,'modwtsqtwolog','s','mln',8,'sym4');
 %     magNoGf = filter(Hlp,magNoG);
 %     magNoGf = magNoG;
     % plot(time,accxf,'r',time,accyf,'g',time,acczf,'b');
