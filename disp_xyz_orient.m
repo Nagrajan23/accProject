be8632578acba9459e5c04c662fc6821b41f4c79
Touqeer Ahmad <touqeer.ahmad@outlook.com>
2017-08-10 16:28:50 -0400

    plots

--- disp_xyz_orient.m
+++ disp_xyz_orient.m
@@ -12,3 +12,36 @@ dist_y = sqrt((x.*x)+(z.*z));
 radians_y = atan(y ./ dist_y);
 degree_y = mod(radtodeg(radians_y), 360);
 
+dist_z = sqrt((x.*x)+(y.*y));
+radians_z = atan(z ./ dist_z);
+degree_z = mod(radtodeg(radians_z), 360);
+
+subplot(2,3,1)
+plot(t,x);
+xlabel('Time (sec)')
+ylabel('X (m/sec)')
+
+subplot(2,3,2)
+plot(t,y);
+xlabel('Time (sec)')
+ylabel('Y (m/sec)')
+
+subplot(2,3,3)
+plot(t,z);
+xlabel('Time (sec)')
+ylabel('Z (m/sec)')
+
+subplot(2,3,4)
+plot(t,degree_x);
+xlabel('Time (sec)')
+ylabel('X Rotation (m/sec)')
+
+subplot(2,3,5)
+plot(t,degree_y);
+xlabel('Time (sec)')
+ylabel('Y Rotation (m/sec)')
+
+subplot(2,3,6)
+plot(t,degree_z);
+xlabel('Time (sec)')
+ylabel('Z Rotation (m/sec)')
